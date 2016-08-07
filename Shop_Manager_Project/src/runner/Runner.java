157255f89bacdadbf5a832f8d2bad93465c9f932
vassmate <vassmate08@gmail.com>
2016-08-07 20:12:02 +0200

    Changing Array to Vector

--- Shop_Manager_Project/src/runner/Runner.java
+++ Shop_Manager_Project/src/runner/Runner.java
@@ -1,5 +1,7 @@
 package runner;
 
+import java.util.Vector;
+
 import products.Milk;
 import shop.Shop;
 
@@ -9,26 +11,25 @@ public class Runner {
 
 		Milk milk1 = new Milk("Falusi", 299, "2016-11-20", 1.5, 20.5);
 		Milk milk2 = new Milk("Riska", 240, "2016-05-10", 1.5, 19.5);
-		Milk milk3 = new Milk("Mizo", 320, "2016-06-15", 1.5, 5.5);
-		Milk milk4 = new Milk("Spar", 199, "2016-08-20", 1.5, 10);
-		Milk milk5 = new Milk("Milk", 209, "2016-12-20", 1.5, 2.5);
-		Milk milk6 = new Milk("Tej", 209, "2016-12-20", 1.5, 2.5);
+		// Milk milk3 = new Milk("Mizo", 320, "2016-06-15", 1.5, 5.5);
+		// Milk milk4 = new Milk("Spar", 199, "2016-08-20", 1.5, 10);
+		// Milk milk5 = new Milk("Milk", 209, "2016-12-20", 1.5, 2.5);
+		// Milk milk6 = new Milk("Tej", 209, "2016-12-20", 1.5, 2.5);
 
-		Milk[] milkArray = { milk1, milk2, milk3, milk4, milk5 };
-
-		Shop milkShop = new Shop("Milk Shop", "Egyenes utca 1.", "Kovacs Jozsi", milkArray);
-		milkShop.replenishMilk(milk6);
-
+		Shop milkShop = new Shop("Milk Shop", "Egyenes utca 1.", "Kovacs Jozsi");
+		milkShop.replenishMilk(milk1);
 		printMilkShop(milkShop);
-		milkShop.buyMilk(milk4);
+		milkShop.replenishMilk(milk2);
 		printMilkShop(milkShop);
-		milkShop.buyMilk(milk1);
-		printMilkShop(milkShop);
-		milkShop.buyMilk(milk6);
-		printMilkShop(milkShop);
+		// milkShop.buyMilk(milk4);
+		// printMilkShop(milkShop);
+		// milkShop.buyMilk(milk1);
+		// printMilkShop(milkShop);
+		// milkShop.buyMilk(milk6);
+		// printMilkShop(milkShop);
 	}
 
-	private static void printMilks(Milk[] milks) {
+	private static void printMilks(Vector<Milk> milks) {
 		for (Milk milk : milks) {
 			System.out.println("\t\t" + milk);
 		}
--- Shop_Manager_Project/src/shop/Shop.java
+++ Shop_Manager_Project/src/shop/Shop.java
@@ -1,5 +1,7 @@
 package shop;
 
+import java.util.Vector;
+
 import products.Milk;
 
 public class Shop {
@@ -7,9 +9,9 @@ public class Shop {
 	private String name;
 	private String address;
 	private String owner;
-	private Milk[] milkBar;
+	private Vector<Milk> milkBar;
 
-	public Shop(String name, String address, String owner, Milk[] milkBar) {
+	public Shop(String name, String address, String owner, Vector<Milk> milkBar) {
 		this.name = name;
 		this.address = address;
 		this.owner = owner;
@@ -34,16 +36,16 @@ public class Shop {
 		return owner;
 	}
 
-	public Milk[] getMilkBar() {
+	public Vector<Milk> getMilkBar() {
 		return milkBar;
 	}
 
-	private void setMilkBar(Milk[] milkBar) {
+	private void setMilkBar(Vector<Milk> milkBar) {
 		this.milkBar = milkBar;
 	}
 
 	public boolean isThereMilk() {
-		if (getMilkBar().length == 0) {
+		if (getMilkBar() == null || getMilkBar().size() == 0) {
 			return false;
 		}
 		return true;
@@ -51,64 +53,30 @@ public class Shop {
 
 	public Milk buyMilk(Milk m) {
 
-		// Make a copy from the milkBar to safely modify and iterate over
-		Milk[] currentMilkBar = getMilkBar().clone();
+		if (isThereMilk()) {
+			@SuppressWarnings("unchecked")
+			Vector<Milk> currentMilkBar = (Vector<Milk>) getMilkBar().clone();
 
-		// Find the sought Milk in the array and make a new array without it
-		for (Milk milk : currentMilkBar) {
-			if (milk.equals(m)) {
-				setMilkBar(makeNewMilkBar(currentMilkBar, m));
-				return milk;
+			if (currentMilkBar.contains(m)) {
+				getMilkBar().remove(m);
+				return currentMilkBar.get(currentMilkBar.indexOf(m));
 			}
+			return getMilkBar().get(0);
 		}
-
-		// If the sought Milk was not found than we just return with the first
-		// one we find and make a new array without it
-		setMilkBar(makeNewMilkBar(currentMilkBar, currentMilkBar[0]));
-		return currentMilkBar[0];
-	}
-
-	private static Milk[] makeNewMilkBar(Milk[] currentMB, Milk milkToExclude) {
-		Milk[] changedMB = new Milk[currentMB.length - 1];
-
-		for (int i = 0; i < changedMB.length; i++) {
-
-			// Check if the current milk is the bought one
-			// If it is than we just add the next milk so we exclude it from the
-			// new array
-			if (currentMB[i].equals(milkToExclude)) {
-				changedMB[i] = currentMB[i + 1];
-			} else {
-				changedMB[i] = currentMB[i];
-			}
-
-			// Check if the previous element is already in the list or not
-			// If it is than we add the next one so there won't be any
-			// duplicates
-			if (i - 1 >= 0) {
-				if (currentMB[i].equals(changedMB[i - 1])) {
-					changedMB[i] = currentMB[i + 1];
-				}
-			}
-		}
-
-		return changedMB;
+		setMilkBar(new Vector<Milk>());
+		return new Milk("No_Milk", 0, "1999-01-01", 0.0, 0.0);
 	}
 
 	public void replenishMilk(Milk m) {
-		if (isThereMilk()) {
-			Milk[] currentMilkBarArray = getMilkBar().clone();
-			Milk[] newMilkBarArray = new Milk[currentMilkBarArray.length + 1];
-
-			for (int i = 0; i < currentMilkBarArray.length; i++) {
-				newMilkBarArray[i] = currentMilkBarArray[i];
+		if (getMilkBar() != null && isThereMilk()) {
+			if (getMilkBar().capacity() - getMilkBar().size() > 0) {
+				getMilkBar().add(m);
+			} else {
+				getMilkBar().addElement(m);
 			}
-
-			newMilkBarArray[newMilkBarArray.length - 1] = m;
-			setMilkBar(newMilkBarArray);
 		} else {
-			setMilkBar(new Milk[1]);
-			getMilkBar()[1] = m;
+			setMilkBar(new Vector<Milk>());
+			getMilkBar().add(m);
 		}
 	}
 
