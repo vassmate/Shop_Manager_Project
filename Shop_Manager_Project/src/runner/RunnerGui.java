d711ae4df46f96f8a1b735ebe9a716c2276c738f
vassmate <vassmate08@gmail.com>
2016-11-07 14:35:38 +0100

    Adding some changes to the userinput

--- Shop_Manager_Project/src/runner/RunnerGui.java
+++ Shop_Manager_Project/src/runner/RunnerGui.java
@@ -18,10 +18,6 @@ import plaza.PlazaImpl;
 import shop.Product;
 import shop.ProductFactory;
 import shop.Shop;
-import shop.Shop.ProductIterator;
-import shop.Shop.ShopRegistration;
-import shop.ShopException;
-import shop.exception.ShopClosedException;
 import shop.product.Milk;
 
 /**
@@ -31,7 +27,7 @@ import shop.product.Milk;
 public class RunnerGui {
 
 	private JFrame frame;
-	private static PlazaImpl centrum = null;
+	private static PlazaImpl plaza = initialzePlaza();
 
 	/**
 	 * Launch the application.
@@ -48,12 +44,11 @@ public class RunnerGui {
 					textArea.setFont(resizedFont);
 					textArea.setEditable(false);
 					window.frame.setVisible(true);
-					window.frame.setTitle("Shop Manager Project Application");
+					window.frame.setTitle("Shop Management Application");
 					window.frame.getContentPane().add(textArea);
 					window.frame.setLocation(0, 0);
 					window.frame.getContentPane().add(new JScrollPane(textArea));
-					textArea.append(buildWelcomeMessage().toString());
-
+					textArea.append(getWelcomeMsg().toString());
 					/**
 					 * Assemble user text field
 					 */
@@ -62,14 +57,17 @@ public class RunnerGui {
 					userText.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent event) {
 							textArea.setText(null);
-							textArea.append(buildWelcomeMessage().toString());
-							centrum = buildPlaza();
-							if (centrum != null) {
-								String[] inputsFromUser = event.getActionCommand().split(" ");
-								for (String userInput : inputsFromUser) {
-									StringBuilder logText = buildLogfileContentText(centrum, userInput);
+							String[] inputsFromUser = event.getActionCommand().split(" ");
+							for (String userInput : inputsFromUser) {
+								if (userInput.equals("help")) {
+									textArea.append(getHelpMsg().toString());
+								} else if (userInput.equals("init.plaza")) {
+									plaza = initialzePlaza();
+									textArea.append("Plaza with shops has been made!");
+								} else {
+									StringBuilder logText = getLogfileContentText(plaza, userInput);
 									if (logText != null) {
-										textArea.append("\n");
+										// textArea.append("\n");
 										textArea.append(logText.toString());
 									}
 								}
@@ -83,6 +81,7 @@ public class RunnerGui {
 				}
 			}
 		});
+
 	}
 
 	/**
@@ -103,72 +102,60 @@ public class RunnerGui {
 		frame.setVisible(true);
 	}
 
-	private static StringBuilder buildWelcomeMessage() {
+	private static StringBuilder getWelcomeMsg() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("\n>Hi! This is a Shop Manager Project Apllictaion."
-				+ "\n>The program has already made a few logfiles at the default log directory which is:"
-				+ "\n\t\tC:/Users/Public/Documents/smp_log" + "\n\tor:" + "\n\t\t/home/smp_log "
-				+ "\n\tif you are using Linux."
-				+ "\n>If you would like to see their content than type a command from below.");
-		sb.append("\n>Available commands:" + "\n\tlog.show = Show all logs."
-				+ "\n\tlog.show.code1 = Show replenish (new product addition and replenishment) logs."
-				+ "\n\tlog.show.code2 = Show remove logs." + "\n\tlog.show.code3 = Show buy logs."
-				+ "\n\tlog.show.code4 = Show log request logs.");
-		sb.append("\n>If you want to clear the logs, than use:" + "\n\tlog.clear");
+		sb.append(">Welcome in the Shop Management Application!");
+		sb.append("\n>The program has already made a few logfiles at the default log directory which is:");
+		sb.append("\n\t\tC:/Users/Public/Documents/smp_log");
+		sb.append("\n\tor:");
+		sb.append("\n\t\t/home/smp_log ");
+		sb.append("\n\tif you are using Linux.\n");
+		sb.append(getHelpMsg());
 		return sb;
 	}
 
-	public static StringBuilder buildShopText(Shop shop, boolean doPrint) {
-		StringBuilder sb = new StringBuilder();
-		try {
-			if (doPrint) {
-				if (shop.isOpen()) {
-					ProductIterator pIter = shop.new ProductIterator(shop.getProductsIterator());
-					sb.append(shop + "\n_Products:");
-					while (pIter.hasNext()) {
-						Product currentIterProduct = (Product) pIter.next();
-						ShopRegistration currentShopReg = shop.getProductStock().get(currentIterProduct.getBarcode());
-						sb.append(currentShopReg);
-					}
-				} else {
-					throw new ShopClosedException();
-				}
-			}
-		} catch (ShopException ex) {
-			sb.append(ex);
-		}
-		return sb;
+	private static StringBuilder getHelpMsg() {
+		StringBuilder sBuilder = new StringBuilder();
+		sBuilder.append(">Available commands:");
+		sBuilder.append("\n\tshow.log - Show all logs.");
+		sBuilder.append("\n\tshow.code1 - Show replenish logs.");
+		sBuilder.append("\n\tshow.code2 - Show remove logs.");
+		sBuilder.append("\n\tshow.code3 - Show buy logs.");
+		sBuilder.append("\n\tshow.code4 - Show log request logs.");
+		sBuilder.append("\n\tclear.log: - Clear the logs.");
+		sBuilder.append("\n\thelp - Show available commands.");
+		return sBuilder;
 	}
 
-	private static StringBuilder buildLogfileContentText(PlazaImpl plaza, String userInput) {
+	private static StringBuilder getLogfileContentText(PlazaImpl plaza, String userInput) {
 		StringBuilder sb = new StringBuilder();
 		Iterator<Shop> plazaIter = plaza.getAllShops();
 		Shop currentShop;
 		Iterator<String> currentShopLogs = null;
 		while (plazaIter.hasNext()) {
 			currentShop = plazaIter.next();
-			sb.append("\n\n>>" + currentShop.getName() + " logfile's content:");
-
-			if (userInput.equals("log.clear")) {
+			if (userInput.equals("clear.log")) {
 				currentShop.clearLogging();
+				sb.append("\n>>" + currentShop.getName() + " logfile's content:");
 				sb.append("\nLogfile cleared!");
 			}
-			if (userInput.equals("log.show")) {
+			if (userInput.equals("show.log")) {
 				currentShopLogs = currentShop.getLogs();
 			}
-			if (userInput.equals("log.show.code1")) {
+			if (userInput.equals("show.code1")) {
 				currentShopLogs = currentShop.getReplenishLogs();
 			}
-			if (userInput.equals("log.show.code2")) {
+			if (userInput.equals("show.code2")) {
 				currentShopLogs = currentShop.getRemoveLogs();
 			}
-			if (userInput.equals("log.show.code3")) {
+			if (userInput.equals("show.code3")) {
 				currentShopLogs = currentShop.getBuyLogs();
 			}
-			if (userInput.equals("log.show.code4")) {
+			if (userInput.equals("show.code4")) {
 				currentShopLogs = currentShop.getProductListRequestLogs();
 			}
 			if (currentShopLogs != null) {
+				sb.append("\n>>" + currentShop.getName() + " logfile's content:");
 				while (currentShopLogs.hasNext()) {
 					sb.append("\n" + currentShopLogs.next());
 				}
@@ -177,20 +164,17 @@ public class RunnerGui {
 		return sb;
 	}
 
-	private static PlazaImpl buildPlaza() {
-		/**
-		 * Make a Plaza with Shops and Products
-		 */
-
+	/**
+	 * Make a Plaza with Shops and Products
+	 */
+	private static PlazaImpl initialzePlaza() {
 		ProductFactory pFactory = new ProductFactory();
-
 		// LongLife milks
 		Product milk1 = pFactory.makeNewLongLifeMilk("Falusi", "2016-11-20", Milk.CUP, Milk.SEMI_SKIMMED);
 		Product milk2 = pFactory.makeNewWholeLLMilk("Riska", "2016-05-10", Milk.HALF_LITER);
 		Product milk3 = pFactory.makeNewSemiSkimmedLLMilk("Riska", "2016-05-10", Milk.LITER);
 		Product milk4 = pFactory.makeNewLiterSemiSkimmedLLMilk("Mizo", "2016-06-15");
 		Product milk5 = pFactory.makeNewLiterWholeLLMilk("Spar", "2016-08-20");
-
 		// SemiLongLife milks
 		Product milk6 = pFactory.makeNewSemiLongLifeMilk("Milk", "2016-12-20", Milk.LITER, Milk.WHOLE);
 		Product milk7 = pFactory.makeNewWholeSLLMilk("Mizo", "2016-05-10", Milk.HALF_LITER);
@@ -198,31 +182,22 @@ public class RunnerGui {
 		Product milk9 = pFactory.makeNewLiterSemiSkimmedSLLMilk("Tej", "2016-12-20");
 		Product milk10 = pFactory.makeNewLiterWholeSLLMilk("Riska", "2016-05-10");
 		Product milk11 = pFactory.makeNewLiterSemiSkimmedSLLMilk("TehenMilk", "2016-05-10");
-
-		// Cheese
+		// Cheeses
 		Product cheese1 = pFactory.makeNewCheese("Tehen", "2016-12-01", 12.6, 10.2);
 		Product cheese2 = pFactory.makeNewCheese("Sajt", "2016-10-01", 11.4, 5.5);
-
-		// Soap
+		// Soaps
 		Product soap1 = pFactory.makeNewSoap("Szappan", 'A');
 		Product soap2 = pFactory.makeNewSoap("Soap", 'B');
-
-		Shop cShop = new Shop("Coop", "Egyenes utca 1.", "K. Jozsi", "C:/Users/Public/Documents");
-		Shop sShop = new Shop("Spar", "Egyenes utca 1.", "S. Jozsi", "C:/Users/Public/Documents");
-		Shop vShop = new Shop("VegyesBolt", "Egyenes utca 1.", "V. Jozsi", "C:/Users/Public/Documents");
-		Shop tShop = new Shop("TheShop", "Egyenes utca 1.", "T. Jozsi", "C:/Users/Public/Documents");
-
-		Vector<Shop> boltok = new Vector<Shop>();
-		boltok.add(cShop);
-		boltok.add(sShop);
-		boltok.add(vShop);
-		boltok.add(tShop);
-
-		PlazaImpl newPlazaObject = new PlazaImpl(boltok);
-
+		// Creating shops and a plaza
+		Vector<Shop> shops = new Vector<Shop>();
+		shops.add(new Shop("Coop", "Egyenes utca 1.", "K. Jozsi", "C:/Users/Public/Documents"));
+		shops.add(new Shop("Spar", "Egyenes utca 1.", "S. Jozsi", "C:/Users/Public/Documents"));
+		shops.add(new Shop("VegyesBolt", "Egyenes utca 1.", "V. Jozsi", "C:/Users/Public/Documents"));
+		shops.add(new Shop("TheShop", "Egyenes utca 1.", "T. Jozsi", "C:/Users/Public/Documents"));
+		PlazaImpl newPlazaObject = new PlazaImpl(shops);
+		// Iterate through the shops and add the products to them
 		Iterator<Shop> shopIter = newPlazaObject.getAllShops();
 		Shop currentShop;
-
 		while (shopIter.hasNext()) {
 			currentShop = shopIter.next();
 			currentShop.open();
@@ -241,15 +216,12 @@ public class RunnerGui {
 			currentShop.addNewProduct(cheese2, 20, 750);
 			currentShop.addNewProduct(soap1, 20, 650);
 			currentShop.addNewProduct(soap2, 20, 450);
-
 			currentShop.buyProduct(milk1.getBarcode(), 5);
 			currentShop.buyProduct(cheese1.getBarcode(), 5);
 			currentShop.buyProduct(soap1.getBarcode(), 5);
-
 			currentShop.removeProduct(milk2.getBarcode());
 			currentShop.removeProduct(cheese2.getBarcode());
 			currentShop.removeProduct(soap2.getBarcode());
-
 			currentShop.replenishProduct(milk1, 6);
 			currentShop.replenishProduct(soap1, 6);
 			currentShop.replenishProduct(cheese1, 6);
